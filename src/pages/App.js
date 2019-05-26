2255b1be44a6f9d0ce02189fe4d83d1b6daf0af7
Raymond Akalonu <raymondosy1@gmail.com>
2019-05-26 02:06:38 +0100

    166255726/chore(test setups): test environment setups - add react-testing-library and enzyme for tests - add travis, code climate, coveralls, enzyme and jest configurations - add test case and snapshot [Finishes #166255726]

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router, Switch, Route
 } from 'react-router-dom';
-import Index from './index';
+import Index from '../components/index';
 
 
 const App = () => (
