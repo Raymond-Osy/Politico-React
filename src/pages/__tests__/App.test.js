890636a6690e6ad0e12b74c284398cac65523f9f
Raymond Akalonu <raymondosy1@gmail.com>
2019-06-03 08:01:19 +0100

    bugfix(test-failings): fix the app failing test [Finishes #166394622]

--- src/pages/__tests__/App.test.js
+++ src/pages/__tests__/App.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter as Router } from 'react-router-dom';
 import { render, cleanup } from 'react-testing-library';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 import App from '../App';
+import { mockStoreData } from '../../__mocks__/mockStore';
+
+const mockStore = configureMockStore([thunk]);
+const store = mockStore(mockStoreData);
+
 
 afterEach(cleanup);
 
 test('<App /> component', () => {
-  const app = render(<App />);
+  const app = render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
   expect(app).toBeTruthy();
   expect(app).toMatchSnapshot();
 });
